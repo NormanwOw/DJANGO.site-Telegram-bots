6b9967589f0832c01a7b3982910eb5577305bd6f
NormanwOw <normjkeee@vk.com>
2023-12-27 22:45:17 +0500

    v0.0.3 -added templates -added css, fonts, images -added views prices, new_order, contacts, order_page and page 404

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,12 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('', views.index, name='home'),
+    path('order-page/', views.order_page, name='order-page'),
+    path('about/', views.about, name='about'),
+    path('prices/', views.prices, name='prices'),
+    path('new-order/', views.new_order, name='new-order'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
+
+handler404 = views.page_not_found
--- main/views.py
+++ main/views.py
@@ -1,10 +1,34 @@
-from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+menu = [{'name': 'О технологии', 'url': 'about'},
+        {'name': 'Цены', 'url': 'prices'},
+        {'name': 'Сделать заказ', 'url': 'new-order'},
+        {'name': 'Контакты', 'url': 'contacts'}]
 
-def index(request: HttpRequest):
-    return render(request, 'main/index.html')
+
+def index(request):
+    return render(request, 'main/index.html', {'title': 'Telegram bots', 'menu': menu})
 
 
-def about(request: HttpRequest):
-    return HttpResponse('About page')
+def about(request):
+    return render(request, 'main/about.html', {'title': 'О технологии', 'menu': menu})
+
+
+def prices(request):
+    return render(request, 'main/prices.html')
+
+
+def new_order(request):
+    return render(request, 'main/new-order.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html', {'title': 'Контакты', 'menu': menu})
+
+
+def order_page(request):
+    return render(request, 'main/order-page.html')
+
+
+def page_not_found(request, exception):
+    return render(request, 'main/page404.html')
