29147a4a0d50ddec48b8a40d7ae2621bf3af12fe
NormanwOw <normjkeee@vk.com>
2024-01-02 12:08:57 +0500

    v0.1.5 -created paginator for my_orders view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from main.models import Order
 
 
 def my_orders(request):
-    pages = ['1', '2', '3', '4']
+
+    page = request.GET.get('page', 1)
+    orders = Order.objects.filter(user=1)
+    paginator = Paginator(orders, 3)
+    current_page = paginator.page(int(page))
+
     context = {
-        'pages': pages,
+        'pages': current_page,
     }
     return render(request, 'users/my-orders.html', context)
