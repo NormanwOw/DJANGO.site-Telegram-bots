91fdba263e0f5cb14306888cd0f740cfb298ee05
NormanwOw <normjkeee@vk.com>
2024-01-06 18:17:34 +0500

    v0.3.1 -added custom error messages for registration form -completed frontend for login and password pages

--- users/forms.py
+++ users/forms.py
@@ -10,12 +10,29 @@ class LoginForm(AuthenticationForm):
 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(min_length=2, max_length=20)
-    last_name = forms.CharField(min_length=2, max_length=20)
-    username = forms.CharField(min_length=3, max_length=16)
+
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=20,
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=20,
+    )
+    username = forms.CharField(
+        min_length=3,
+        max_length=16,
+    )
     email = forms.EmailField()
     password1 = forms.PasswordInput()
     password2 = forms.PasswordInput()
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['first_name'].error_messages.update(self.length_msg(2, 20))
+        self.fields['last_name'].error_messages.update(self.length_msg(2, 20))
+        self.fields['username'].error_messages.update(self.length_msg(3, 16))
 
     class Meta:
         model = User
@@ -27,3 +44,10 @@ class RegistrationForm(UserCreationForm):
             'password1',
             'password2',
         )
+
+    @staticmethod
+    def length_msg(min_len, max_len: str) -> dict:
+        return {
+            'min_length': f'Минимальноe количество символов - {min_len}',
+            'max_length': f'Максимальное количество символов - {max_len}'
+        }
