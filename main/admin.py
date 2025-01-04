2496ccf840c7899e08d27ebbc119b52c636d0867
NormanwOw <normjkeee@vk.com>
2025-01-04 18:48:16 +0500

    v0.9.6.8 -updated repositories implementation in main app

--- main/admin.py
+++ main/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
-from main.models import Order, Product
+from main.infrastructure.models import OrderModel, ProductModel
 
 
 class OrderTabularAdmin(admin.TabularInline):
-    model = Order
+    model = OrderModel
 
     readonly_fields = ('date',)
     extra = 1
 
 
-@admin.register(Order)
+@admin.register(OrderModel)
 class OrderAdmin(admin.ModelAdmin):
     list_display = 'order_id', 'user', 'phone_number', 'total_price', 'status', 'date'
     search_fields = ('order_id',)
 
 
-@admin.register(Product)
+@admin.register(ProductModel)
 class ProductAdmin(admin.ModelAdmin):
     list_display = 'title', 'price', 'description'
     search_fields = ('title',)
-
--- main/infrastructure/repositories.py
+++ main/infrastructure/repositories.py
@@ -1,48 +1,106 @@
-from django.core.exceptions import ObjectDoesNotExist
+from typing import List
 
-from main.infrastructure.interfaces import IUserRepository, AbstractRepository, IOrderRepository, \
-    IProductRepository
-from main.models import OrderModel, ProductModel
-from users.models import User
+from main.domain.aggregates import Order
+from main.domain.entities import Product
+from main.infrastructure.interfaces import IUserRepository, IOrderRepository, IProductRepository
+from main.infrastructure.models import OrderModel, ProductModel
+from users.domain.entities import User
+from users.models import UserModel
 
 
-class DjangoRepository(AbstractRepository):
-    model = None
-
-    @classmethod
-    def get(cls, pk):
-        try:
-            return cls.model.objects.get(pk=pk)
-        except ObjectDoesNotExist:
-            return None
+class UserRepository(IUserRepository):
+    model = UserModel
 
     @classmethod
-    def create(cls, **kwargs):
-        return cls.model.objects.create(**kwargs)
+    def get(cls, user: User) -> UserModel:
+        return cls.model.objects.get(pk=user.id)
 
     @classmethod
-    def update(cls, instance, **kwargs):
-        for key, value in kwargs.items():
-            setattr(instance, key, value)
-        instance.save()
-        return instance
+    def create(cls, user: User) -> UserModel:
+        raise NotImplementedError
 
     @classmethod
-    def delete(cls, instance):
-        instance.delete()
+    def update(cls, user: User, user_model: UserModel) -> UserModel:
+        user_model.username = user.username
+        user_model.email = user.email
+        user_model.first_name = user.first_name
+        user_model.last_name = user.last_name
+        user_model.is_active = user.is_active
+        user_model.is_staff = user.is_staff
+        user_model.is_superuser = user.is_superuser
+        user_model.save()
+        return user_model
 
     @classmethod
-    def all(cls):
+    def delete(cls, user_model: UserModel):
+        cls.model.delete(user_model)
+
+    @classmethod
+    def all(cls) -> List[UserModel]:
         return cls.model.objects.all()
 
 
-class UserRepository(DjangoRepository, IUserRepository):
-    model = User
-
-
-class OrderRepository(DjangoRepository, IOrderRepository):
+class OrderRepository(IOrderRepository):
     model = OrderModel
 
+    @classmethod
+    def get(cls, order: Order) -> OrderModel:
+        return cls.model.objects.get(order_id=order.number)
 
-class ProductRepository(DjangoRepository, IProductRepository):
+    @classmethod
+    def create(cls, order: Order) -> OrderModel:
+        order_model = cls.model.from_domain(order)
+        order_model.save()
+        return order_model
+
+    @classmethod
+    def update(cls, order: Order, order_model: OrderModel) -> OrderModel:
+        order_model.order_id = order.number
+        order_model.phone_number = order.phone_number
+        order_model.total_price = order.total_price
+        order_model.save()
+        return order_model
+
+    @classmethod
+    def delete(cls, order_model: OrderModel):
+        cls.model.delete(order_model)
+
+    @classmethod
+    def all(cls) -> List[OrderModel]:
+        return cls.model.objects.all()
+
+
+class ProductRepository(IProductRepository):
     model = ProductModel
+
+    @classmethod
+    def get(cls, product: Product) -> ProductModel:
+        return cls.model.objects.get(code=product.code)
+
+    @classmethod
+    def get_by_code(cls, code_list: list[str]) -> List[ProductModel]:
+        return cls.model.objects.filter(code__in=code_list)
+
+    @classmethod
+    def create(cls, product: Product) -> ProductModel:
+        product_model = cls.model.from_domain(product)
+        product_model.save()
+        return product_model
+
+    @classmethod
+    def update(cls, product: Product, product_model: ProductModel) -> ProductModel:
+        product_model.code = product.code
+        product_model.name = product.name
+        product_model.price = product.price
+        product_model.title = product.title
+        product_model.description = product.description
+        product_model.save()
+        return product_model
+
+    @classmethod
+    def delete(cls, product_model: ProductModel):
+        cls.model.delete(product_model)
+
+    @classmethod
+    def all(cls) -> List[ProductModel]:
+        return cls.model.objects.all()
