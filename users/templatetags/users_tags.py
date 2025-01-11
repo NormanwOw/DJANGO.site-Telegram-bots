372012423d3b3573187729d9d7361d8bed62d7a0
NormanwOw <normjkeee@vk.com>
2025-01-11 12:38:46 +0500

    v0.9.7.5 -removed get_order_page tag from users_tags

--- users/templatetags/users_tags.py
+++ users/templatetags/users_tags.py
@@ -1,10 +1,5 @@
 from django import template
-from django.core.paginator import Paginator
-from django.db.models import Q
 from django.utils.http import urlencode
-from django.shortcuts import Http404
-
-from main.infrastructure.models import OrderModel
 
 register = template.Library()
 
@@ -15,28 +10,3 @@ def change_params(context, **kwargs):
     query.update(kwargs)
 
     return '?' + urlencode(query)
-
-
-@register.simple_tag(takes_context=True)
-def get_order_page(context):
-    request = context['request']
-    order_id = request.GET.get('remove-order', False)
-
-    if order_id:
-        result = OrderModel.objects.filter(order_id=order_id, user=request.user).delete()
-
-        if not result[0]:
-            raise Http404()
-
-    page = request.GET.get('page', 1)
-    order_number = request.GET.get('search', False)
-    query = Q(user=request.user.id)
-
-    if order_number:
-        query &= Q(order_id__contains=order_number)
-
-    orders = OrderModel.objects.filter(query)
-    paginator = Paginator(orders, 3)
-    current_page = paginator.page(int(page))
-
-    return current_page
