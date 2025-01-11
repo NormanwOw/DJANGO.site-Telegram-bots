b71fc064f18179716a59a66baf3745d4724a0d15
NormanwOw <normjkeee@vk.com>
2025-01-11 12:41:05 +0500

    v0.9.7.7 -updated CreateOrder command

--- main/application/services/commands/create_order.py
+++ main/application/services/commands/create_order.py
@@ -7,7 +7,10 @@ class CreateOrder(CreateCommand):
 
     def __call__(self, uow: IUnitOfWork, order: Order):
         with uow:
+            bot_shop = uow.product_repository.get_by_name('bot_shop')
+            order.total_price += bot_shop.price
             order_model = uow.order_repository.create(order)
             code_list = [product.code for product in order.products]
-            products = uow.product_repository.get_by_code(code_list)
+            products = list(uow.product_repository.get_by_code(code_list))
+            products.append(bot_shop)
             order_model.products.add(*products)
